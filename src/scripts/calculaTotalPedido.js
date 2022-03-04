82df0ed4680b242c20f3e5b7930875c7f835f664
izabela-dsn <izabeladasilva10@gmail.com>
2022-03-04 15:58:54 -0300

    script para calcular o total do pedido

---
+++ src/scripts/calculaTotalPedido.js
@@ -0,0 +1,8 @@
+export const calculaTotalPedido = valores => {
+  /*
+  const qtd = parseInt(qtdDePratos)
+  const precoPrato = parseFloat(precoDoPrato.replace("R$ ", " ")).toFixed(2)
+  */
+
+  console.log(valores)
+}
