00db9270834a07147be4b220f0ea331cf0325e1b
Izabela-dsn <izabeladasilva10@gmail.com>
2022-03-05 20:59:00 -0300

    script para escolha de forma de pagamento

---
+++ src/scripts/formaPagamento.js
@@ -0,0 +1,25 @@
+//pegar valor total
+const mesa = JSON.parse(localStorage.getItem("Mesa")) || []
+const totalDaMesa = mesa[0].total
+
+const campoValorTotal = document.querySelector(".item-pedido-valor")
+campoValorTotal.innerHTML = `R$ ${totalDaMesa.toFixed(2)}`
+
+//botões de pagamento
+const btns = document.querySelectorAll("button")
+const btnPix = document.querySelector(".btn-pix")
+const btnDinhCartao = document.querySelector(".btn-cartao-dinheiro")
+const pix = document.querySelector(".pix")
+const dinheiroCartao = document.querySelector(".cartao-dinheiro")
+
+btnPix.onclick = (e)=>{
+    e.preventDefault()
+    pix.classList.add("active")
+    dinheiroCartao.classList.remove("active")
+}
+
+btnDinhCartao.onclick = (e)=>{
+    e.preventDefault()
+    pix.classList.remove("active")
+    dinheiroCartao.classList.add("active")
+}
