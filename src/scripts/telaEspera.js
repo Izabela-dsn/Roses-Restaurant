c5b251f09a9056ef9135c7982c8dfb8e1a6ee475
Izabela-dsn <izabeladasilva10@gmail.com>
2022-03-05 20:55:29 -0300

    add qtd de pedido para o user ver e melhorias

--- src/scripts/telaEspera.js
+++ src/scripts/telaEspera.js
@@ -4,33 +4,42 @@ const mesa = JSON.parse(localStorage.getItem("Mesa")) || []
 const nomeMesa = document.querySelector(".nome-mesa")
 const listaPedido = document.querySelector("ul")
 var valores = []
-const pedido = (nomePrato, valor) => {
+const pedido = (nomePrato, valor, qtd) => {
   const novoPedido = document.createElement("li")
   novoPedido.classList.add("item-pedido")
   const pedido = `
-    <p>${nomePrato}</p>
+    <p>${qtd}x ${nomePrato}</p>
     <p>${valor}</p>  
   `
   novoPedido.innerHTML = pedido
   return novoPedido
 }
 
-nomeMesa.innerHTML = `Mesa ${mesa[0].nomeDaMesa}`
+nomeMesa.innerHTML = `Mesa ${mesa[mesa.length-1].nomeDaMesa}`
 
 const carregaPedido = () => {
-  mesa.forEach(item => {
-    const pedidos = item.pedido[0]
-    pedidos.forEach(itemPedido => {
-      //console.log(itemPedido)
-      const precoDoPrato = itemPedido.precoDoPrato
-      const nomeDoPrato = itemPedido.nomeDoPrato
-      const qtdDoPrato = itemPedido.qtdDePratos
-      valores.push(qtdDoPrato, precoDoPrato)
+  try {
+    mesa.forEach(item => {
+      const pedidos = item.pedido
 
-      listaPedido.appendChild(pedido(nomeDoPrato, precoDoPrato))
+      pedidos.forEach(itemPedido => {
+        for(var item of itemPedido){
+          console.log(item.nomeDoPrato)
+          const precoDoPrato = item.precoDoPrato
+          const nomeDoPrato = item.nomeDoPrato
+          const qtdDoPrato = item.qtdDePratos
+          valores.push({qtdDoPrato:qtdDoPrato, precoDoPrato:precoDoPrato})
+          listaPedido.appendChild(pedido(nomeDoPrato, precoDoPrato, qtdDoPrato))
+        }
+  
+      })
+      //console.log(valores)
+      calculaTotalPedido(valores)
     })
-    console.log(valores)
-    calculaTotalPedido(valores)
-  })
+  } catch {
+    alert(
+      "Estamos com problemas técnicos por favor chame um de nossos colaboradores."
+    )
+  }
 }
 carregaPedido()
