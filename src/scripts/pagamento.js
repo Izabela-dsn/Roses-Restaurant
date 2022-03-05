858d26583c2f1655ccef3960b7356172d58a175d
Izabela-dsn <izabeladasilva10@gmail.com>
2022-03-05 20:54:04 -0300

    exclusão de script

--- src/scripts/pagamento.js
+++
@@ -1,8 +0,0 @@
-//pegar valor total
-const mesa = JSON.parse(localStorage.getItem("Mesa")) || []
-const totalDaMesa = mesa[0].total
-
-const campoValorTotal = document.querySelector(".item-pedido-valor")
-campoValorTotal.innerHTML = `R$ ${totalDaMesa.toFixed(2)}`
-
-//botões de pagamento
