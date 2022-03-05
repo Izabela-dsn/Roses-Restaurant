0b0a2fc14c2724a9dc11c4ea65e0ffa9a9495cad
Izabela-dsn <izabeladasilva10@gmail.com>
2022-03-05 20:56:00 -0300

    script para calcular o total do pedido

--- src/scripts/calculaTotalPedido.js
+++ src/scripts/calculaTotalPedido.js
@@ -1,8 +1,26 @@
 export const calculaTotalPedido = valores => {
-  /*
-  const qtd = parseInt(qtdDePratos)
-  const precoPrato = parseFloat(precoDoPrato.replace("R$ ", " ")).toFixed(2)
-  */
-
   console.log(valores)
+
+  const mesa = JSON.parse(localStorage.getItem("Mesa")) || []
+  var novosValores = []
+  try {
+    valores.map(function(valor){
+      var valorPrato = parseFloat((valor.precoDoPrato).replace("R$ ", " ")).toFixed(2)
+      var qtd = parseInt(valor.qtdDoPrato)
+      var valoresDoPedido = qtd * valorPrato
+      console.log(valoresDoPedido)
+      novosValores.push(valoresDoPedido)
+    })
+    console.log(novosValores)
+    var totalPedido = novosValores.reduce((totalPedido,novosValores)=> totalPedido + novosValores,0).toFixed(2)
+    console.log(totalPedido)
+    mesa[mesa.length-1].total = totalPedido
+    localStorage.setItem("Mesa", JSON.stringify(mesa))
+    
+  } catch  {
+    alert(
+      "Estamos com problemas técnicos por favor chame um de nossos colaboradores."
+    )
+  }
 }
+
